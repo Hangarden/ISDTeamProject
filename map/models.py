b6bc95b1794b4d304e624a46c287877c7e984a29
Jiwon <jeewonre@naver.com>
2021-06-06 22:09:37 +0900

    city table data create

--- map/models.py
+++ map/models.py
@@ -4,6 +4,6 @@ from django.contrib.gis.db import models
 class MapCity(models.Model):
     id = models.BigAutoField(primary_key=True)
     sigungu_en = models.CharField(db_column='sigungu_EN', max_length=200)  # Field name made lowercase.
-    sigungu_kr = models.DateTimeField(db_column='sigungu_KR')  # Field name made lowercase.
+    sigungu_kr = models.CharField(db_column='sigungu_KR', max_length=200)  # Field name made lowercase.
     geometry = models.GeometryField()
 
--- map/views.py
+++ map/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from django.contrib.gis.geos import GEOSGeometry
+from map.models import MapCity
+import json
 # Create your views here.
+
+
+def create_city(request):
+    with open('./seoul.geojson') as json_file:
+        json_data = json.load(json_file)
+        for data in json_data['features']:
+            print(data)
+            pnt = GEOSGeometry(str(data['geometry']))
+            city = MapCity(sigungu_en= data['properties']['SIG_ENG_NM'], sigungu_kr=data['properties']['SIG_KOR_NM'], geometry=pnt)
+            city.save()
+    return render(request)
